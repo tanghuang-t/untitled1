6546d0a8930e410027fab7931e10087b823feb51
tanghuang-t <82073059+tanghuang-t@users.noreply.github.com>
2021-10-07 02:12:29 -0500

    Add files via upload

---
+++ JDBCUtil.java
@@ -0,0 +1,56 @@
+package com;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCUtil {
+
+    public static void release(Connection conn,Statement st,ResultSet rs) {
+        closeRs(rs);
+        closeSt(st);
+        closeConn(conn);
+    }
+
+    private static void closeRs(ResultSet rs) {
+        try {
+            if(rs!=null) {
+                rs.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            rs=null;
+        }
+
+    }
+
+    private static void closeSt(Statement st) {
+        try {
+            if(st!=null) {
+                st.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            st=null;
+        }
+
+    }
+
+    private static void closeConn(Connection conn) {
+        try {
+            if(conn!=null) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            conn=null;
+        }
+
+    }
+
+
+}
